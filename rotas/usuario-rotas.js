83995377063f2158c4c654783ae1cf8da36d03d5
maikelsouza <maikel.souza@gmail.com>
2020-01-09 00:04:00 -0200

    Novo teste

--- rotas/usuario-rotas.js
+++ rotas/usuario-rotas.js
@@ -12,7 +12,6 @@ rota.post('/autenticar', _ctrl.autenticar);
 //rota.get("/register", _ctrl.post);
 
 
-
 rota.get("/",autenticador,_ctrl.get);
 rota.get('/:id',autenticador, _ctrl.getById);
 rota.post('/',  _ctrl.post);
