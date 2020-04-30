c3225277b3567104e6b8ef4a823606d00b07a1f5
maikelsouza <maikel.souza@gmail.com>
2020-04-30 00:31:54 -0300

    Adição principais tipos estabelecimentos

---
+++ db/seeders/20200430032218-tipoEstabelecimento.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {    
+      return queryInterface.bulkInsert('TipoEstabelecimentos', [{
+        nome: 'Barbearia',
+        createdAt: new Date(),
+        updatedAt: new Date()},
+        {nome: 'Café',
+        createdAt: new Date(),
+        updatedAt: new Date()},
+        {nome: 'Clínica Estética',
+        createdAt: new Date(),
+        updatedAt: new Date()},                
+        {nome: 'Pet',
+        createdAt: new Date(),
+        updatedAt: new Date()},        
+        {nome: 'Lavação',
+        createdAt: new Date(),
+        updatedAt: new Date()},
+        {nome: 'Loja Roupa',
+        createdAt: new Date(),
+        updatedAt: new Date()},
+        {nome: 'Pizzaria',
+        createdAt: new Date(),
+        updatedAt: new Date()},
+        {nome: 'Salão Beleza',
+        createdAt: new Date(),
+        updatedAt: new Date()                
+      }], {});
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
