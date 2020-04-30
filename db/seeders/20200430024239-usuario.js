7b33c07a6a5276092acda74b25577fc9ec612d30
maikelsouza <maikel.souza@gmail.com>
2020-04-29 23:57:41 -0300

    Criação seed usuário admin

---
+++ db/seeders/20200430024239-usuario.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Usuarios', [{
+        nome: 'Maikel Joel de Souza',
+        sexo: 'Masculino',
+        email: 'maikel.souza@gmail.com',
+        dataNascimento: '1979-02-09 00:00:00',
+        senha: '26ebbd10de57c7c78d70169dfb3bd69e',
+        cpf: '00342683918',
+        grupoUsuarioId: 1,
+        ativo: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
