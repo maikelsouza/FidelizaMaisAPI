b7953d2e3ca98c294c2fdaf294382c4c56409fcf
maikelsouza <maikel.souza@gmail.com>
2020-05-30 10:09:24 -0300

    Mudanca Conf, por conta mudaça nome

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const app = require('../CartaoFidelidadeAPI/bin/express');
-const variaveis = require('../CartaoFidelidadeAPI/bin/configuracoes/variaveis');
+const app = require('../FidelizaMaisAPI/bin/express');
+const variaveis = require('../FidelizaMaisAPI/bin/configuracoes/variaveis');
 
 app.listen(variaveis.Api.port, () => {
     console.info(`Api inicializada com sucesso na porta ${variaveis.Api.port}`);
