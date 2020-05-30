f41396cc20079874b0d2796497437fb6e01e2b57
maikelsouza <maikel.souza@gmail.com>
2020-05-30 15:49:57 -0300

    novo teste

--- rotas/usuario-rotas.js
+++ rotas/usuario-rotas.js
@@ -17,6 +17,6 @@ rota.get('/:id',autenticador, _ctrl.getById);
 rota.post('/',  autenticador,_ctrl.post);
 rota.put('/:id',autenticador, _ctrl.update);
 rota.put('/atualizarSenha/:id',autenticador, _ctrl.updateSenha);
-//rota.delete('/:id', _ctrl.delete);
+
 
 module.exports = rota;
