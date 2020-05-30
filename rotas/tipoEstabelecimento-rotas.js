b1a5ba16d99b3a752f9ff9b771bd8208b3a83adb
maikelsouza <maikel.souza@gmail.com>
2020-05-30 16:06:11 -0300

    Mais um teste

--- rotas/tipoEstabelecimento-rotas.js
+++ rotas/tipoEstabelecimento-rotas.js
@@ -10,6 +10,6 @@ let _ctrl = new controle();
 rota.get("/",autenticador, _ctrl.buscarTodosAtivos);
 //rota.get('/:id',_ctrl.getById);
 //rota.post('/', _ctrl.post);
-//rota.put('/:id', _ctrl.put);
+
 
 module.exports = rota;
