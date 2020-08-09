7ce08bf080809680ab7d1b2e9813c88ae4747faf
maikelsouza <maikel.souza@gmail.com>
2020-08-09 17:56:12 -0300

    Adicionado unique campo cpnj

--- db/migrations/20190501013715-create-estabelecimento.js
+++ db/migrations/20190501013715-create-estabelecimento.js
@@ -16,7 +16,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       cnpj: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       ativo: {
         type: Sequelize.BOOLEAN,
