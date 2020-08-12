bc23c54c1a2c1a5e825b2b4bfaf7545e486d8b2c
maikelsouza <maikel.souza@gmail.com>
2020-08-12 15:00:48 -0300

    melhoria ativo

---
+++ repositorios/totalPontosClienteProgramaFidelidade-repositorio (1).js
@@ -0,0 +1,49 @@
+const model = require('../config/modelLoader');
+const Op = model.Sequelize.Op
+
+class totalPontosClienteProgramaFidelidadeRepositorio{
+
+    constructor(){}
+
+
+    async create(data){          
+        model.TotalPontosClienteProgramaFidelidade.create(data,
+            {include: 
+               [ model.PontosClienteProgramaFidelidade]                       
+            }
+         );
+     }    
+
+     async update(id, data) {
+      model.TotalPontosClienteProgramaFidelidade.update(data,{
+          where: {id}   
+      });
+  }
+
+    async getUsuarioIdProgramaFidelidadeIdAtivo(usuarioId, programaFidelidadeId) {
+      return await model.
+         TotalPontosClienteProgramaFidelidade.findOne({
+                  attributes: ['id', 'totalPontos'],
+                  where: {  
+                     [Op.and]: 
+                     [{usuarioId}, {ativo: true}, {programaFidelidadeId}
+                    ]
+               },    
+            });
+    }    
+
+    async buscarPorIdUsuarioEAtivo(usuarioId, ativo) {
+        return await model.
+           TotalPontosClienteProgramaFidelidade.findAll({
+                    attributes: ['id', 'usuarioId', 'programaFidelidadeId','totalPontos'],
+                    where: {  
+                       [Op.and]: 
+                       [{usuarioId}, {ativo}
+                      ]
+                 },    
+              });
+      }    
+     
+}
+
+module.exports = totalPontosClienteProgramaFidelidadeRepositorio;
